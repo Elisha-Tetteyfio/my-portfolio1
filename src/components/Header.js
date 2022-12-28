050796e0a589ee0be7db5711062a97cab40f95ea
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-12-28 10:07:00 +0000

    Add intro page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ import styles from "./Header.module.css"
 const Header = () => {
   return(
     <div className={styles.header}>
-      <div className={styles.header1}>Elisha</div>
-      <div className={styles.header2}>
-        <div className={styles.headerLink}>Home</div>
-        <div className={styles.headerLink}>About</div>
-        <div className={styles.headerLink}>Projects</div>
-        <div className={styles.headerLink}>Testimonials</div>
-        <div className={styles.headerLink}>Contact me</div>
+      <div className={styles.headerr}>
+        <div className={styles.header1}>Elisha</div>
+        <div className={styles.header2}>
+          <div className={styles.headerLink}>Home</div>
+          <div className={styles.headerLink}>About</div>
+          <div className={styles.headerLink}>Projects</div>
+          <div className={styles.headerLink}>Testimonials</div>
+          <div className={styles.headerLink}>Contact me</div>
+        </div>
       </div>
     </div>
   )
--- src/segments/Intro.js
+++ src/segments/Intro.js
@@ -1,14 +1,43 @@
 import React from "react";
 import styles from "./Intro.module.css"
-import im from "../images/t4.png"
+import im from "../images/t1.png"
 
 const Intro = () => {
   return(
-    <div className={styles.intro}>
-      <h2 className={styles.intro1}>Hello there, I am... </h2>
-      <h1 className={styles.intro2}>Elisha Tetteyfio</h1>
-      <h3 className={styles.intro3}>...a full-stack developer</h3>
-      {/* <img src={im} alt="fsd"/> */}
+    <div>
+      <div className={styles.intro}>
+        <div className={styles.introLeft}>
+          <div className={styles.intro1}>Hello there,</div>
+          <div className={styles.intro2}>I am <span className={styles.introName}>Elisha Tetteyfio</span></div>
+          <div className={styles.intro3}>I am a full-stack developer and a mathematician too. Feel free to contact me about Programming or Maths.</div>
+          <div>
+            <button className={`${styles.introButton} ${styles.introButton1}`}>Hire me</button>
+            <button className={styles.introButton}>Let's talk</button>
+          </div>
+        </div>
+        <div className={styles.introRight}>
+          <img src={im} alt="My image"/>
+        </div>
+      </div>
+      <div className={styles.introDown}>
+        <div className={styles.introDown1}>
+          <div>
+            Hello &#x2727;
+          </div>
+          <div className={styles.introDown2}>
+            &#x2727;
+          </div>
+          <div>
+            Hello &#x2727;
+          </div>
+          <div className={styles.introDown2}>
+            &#x2727;
+          </div>
+          <div>
+            Hello &#x2727;
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
