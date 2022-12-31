a8bd171ba3f78dacbeb41428a8c07303944262d2
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-12-31 00:22:02 +0000

    Add some `medium-sized screen`  design

--- src/segments/Intro.js
+++ src/segments/Intro.js
@@ -9,14 +9,17 @@ const Intro = () => {
         <div className={styles.introLeft}>
           <div className={styles.intro1}>Hello there,</div>
           <div className={styles.intro2}>I am <span className={styles.introName}>Elisha Tetteyfio</span></div>
-          <div className={styles.intro3}>I am a full-stack developer and a mathematician too. Feel free to contact me about Programming or Maths.</div>
+          <div className={styles.intro3}>
+            I am a full-stack developer and a mathematician too. 
+            <span className={styles.deskOnly}>Feel free to contact me about Programming or Maths.</span>
+          </div>
           <div>
             <button className={`${styles.introButton} ${styles.introButton1}`}>Hire me</button>
             <button className={styles.introButton}>Let's talk</button>
           </div>
         </div>
         <div className={styles.introRight}>
-          <img src={im} alt="My image"/>
+          <img src={im} alt="Me" className={styles.deskOnly}/>
         </div>
       </div>
       <div className={styles.introDown}>
