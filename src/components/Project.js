85dd1b78a941e6b862edd9183cf2ff939b66d842
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-12-31 14:38:04 +0000

    Add project images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./Project.module.css"
-import im from "../images/aboutImage.jpg"
 
 const Project = (project) => {
   return (
     <div className={styles.card}>
-      <div className={styles.imgDiv}> <img src={im} alt="im" /> </div>
+      <div className={styles.imgDiv}> <img src={project.im} alt="im" /> </div>
       <div className={styles.projectInfo}>
         <div className={styles.projectName}>{project.name}</div>
         <div className={styles.projectDetails}>
--- src/segments/Projects.js
+++ src/segments/Projects.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Project from "../components/Project";
 import styles from "./Projects.module.css"
+import math from "../images/projects/mathmagicians.png"
+import bookdev from "../images/projects/bookadev.png"
+import todo from "../images/projects/todolist.png"
+import mentors from "../images/projects/meetmentors.png"
+import leaders from "../images/projects/leaderboard.png"
+import portfolio from "../images/projects/portfolio.png"
 
 const Projects = () => {
   return (
@@ -9,22 +15,35 @@ const Projects = () => {
       <div className={styles.projectsSection}>
         <Project 
           name="Math magician"
-          description="Project description is a little description about the project to show you"
+          description="A fun website for all mathematics lovers to perform calculations and get math quotes."
+          im={math}
         />
         <Project 
-          name="Budget app"
+          name="Book A Dev"
           description="The budget app allows users to keep records of financial transactions they've made an plan
           their budget well"
+          im={bookdev}
         />
         <Project 
           name="To-do list"
           description="An app to help you schedule your daily activities"
+          im={todo}
         />
         <Project 
-          name="Space hub"
+          name="Meet Mentors"
+          description="A website giving information about Meet Mentors programme"
+          im={mentors}
         />
-        <Project />
-        <Project />
+        <Project
+          name="Leaderboard"
+          description="A site connected to an API to allow gamers record their scores and view other player's scores."
+          im={leaders}  
+        />
+        <Project
+          name="Portfolio"
+          description="This project was my previous portfolio"
+          im={portfolio}
+        />
       </div>
     </div>
   )
