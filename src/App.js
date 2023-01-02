e05d205caef3727a1c53917f38f554fe14f383a1
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2023-01-02 02:14:44 +0000

    Add `dark` class with template colors

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Contact from "./segments/Contact";
 import Expertise from "./segments/Expertise";
 import Intro from "./segments/Intro";
 import Projects from "./segments/Projects";
+import styles from "./App.module.css"
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const isLight = useSelector(state=>state.isLightTheme);
+
   return (
-    <div >
+    <div className={`${!isLight? `${styles.dark}`:""} ${styles.all}`}>
       <Header />
       <Intro />
       <About />
